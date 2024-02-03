083d0c1bfb0744ed6f0fa54258be97f57f891328
ryan2376 <ryannesh2376@gmail.com>
2024-02-03 09:54:22 +0300

    db conn

---
+++ backend/db_connect.php
@@ -0,0 +1,26 @@
+<?php
+// Handle CORS
+header("Access-Control-Allow-Origin: *"); // Adjust this depending on your security requirements
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Database configuration
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "saveformdata";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+// Close the database connection
+$conn->close();
+
+?>
+
+
