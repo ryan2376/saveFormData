72f7449a9ee6993f1edf59588ede444f14764c86
ryan2376 <ryannesh2376@gmail.com>
2024-03-07 14:10:54 +0300

    NAVBAR2

--- frontend/my-react-app/src/MyNavbar.js
+++ frontend/my-react-app/src/MyNavbar.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { Navbar as FlowbiteNavbar, Button as FlowbiteButton } from 'flowbite-react';
-import { Avatar, Dropdown } from 'flowbite-react';
+import { Navbar as FlowbiteNavbar } from 'flowbite-react';
 
 const MyNavbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,27 +10,26 @@ const MyNavbar = () => {
         setIsOpen(!isOpen);
     };
 
-
-    
     return (
         <FlowbiteNavbar fluid rounded>
-        <FlowbiteNavbar.Brand href="https://flowbite-react.com">
-        <img src="favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> 
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Activist Connect</span>
-        </FlowbiteNavbar.Brand>
-        <div className="flex md:order-2">
-        <FlowbiteButton>Get started</FlowbiteButton>
-        <FlowbiteNavbar.Toggle />
-        </div>
-        <FlowbiteNavbar.Collapse>
-        <FlowbiteNavbar.Link as={Link} to="/"active>Home</FlowbiteNavbar.Link>
-        <FlowbiteNavbar.Link href="#">About</FlowbiteNavbar.Link>
-        <FlowbiteNavbar.Link as={Link} to="/activist-login">Activist</FlowbiteNavbar.Link>
-        <FlowbiteNavbar.Link as={Link} to="/institution-login">Institution</FlowbiteNavbar.Link>
-        <FlowbiteNavbar.Link href="#">Contact</FlowbiteNavbar.Link>
-        </FlowbiteNavbar.Collapse>
-    </FlowbiteNavbar>
+            <FlowbiteNavbar.Brand href="https://flowbite-react.com">
+                <img src="favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> 
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Activist Connect</span>
+            </FlowbiteNavbar.Brand>
+            <FlowbiteNavbar.Collapse className={isOpen ? 'nav-links open' : 'nav-links'}>
+                <FlowbiteNavbar.Link as={Link} to="/" className="nav-link">Home</FlowbiteNavbar.Link>
+                <FlowbiteNavbar.Link href="#" className="nav-link">About</FlowbiteNavbar.Link>
+                <FlowbiteNavbar.Link as={Link} to="/activist-login" className="nav-link">Activist</FlowbiteNavbar.Link>
+                <FlowbiteNavbar.Link as={Link} to="/institution-login" className="nav-link">Institution</FlowbiteNavbar.Link>
+                <FlowbiteNavbar.Link href="#" className="nav-link">Contact</FlowbiteNavbar.Link>
+            </FlowbiteNavbar.Collapse>
+            <button className="nav-toggle" onClick={toggleNav}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </button>
+        </FlowbiteNavbar>
     );
-    }
+}
 
 export default MyNavbar;
