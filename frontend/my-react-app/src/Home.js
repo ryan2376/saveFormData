8623b6a36b9df18d941517233c264073e96dba78
ryan2376 <ryannesh2376@gmail.com>
2024-03-11 11:56:21 +0300

    adjustments1

--- frontend/my-react-app/src/Home.js
+++ frontend/my-react-app/src/Home.js
@@ -7,19 +7,26 @@ import differenceImage from './images/michael-krahn-eGD69I3ODC4-unsplash.jpg';
 const Home = () => {
     return (
         <div className="home-container">
+            {/* <img src={differenceImage} alt="difference" /> */}
+
             <section className="bg-white dark:bg-gray-900">
                 <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                     <div className="mr-auto place-self-center lg:col-span-7">
                         <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">WELCOME TO ACTIVIST CONNECT</h1>
                         <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 text-center">Connecting activists with institutions for a greener world.</p>
                         
-            
                     </div>
                     <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                    <img src={heroImage} alt="Hero" />
-                    </div>                
+                    <img src={projectImage} alt="Project" />
+                    </div>  
+                    <div className="wave-container">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <path fill="#0099ff" fillOpacity="1" d="M0,96L40,112C80,128,160,160,240,144C320,128,400,64,480,58.7C560,53,640,107,720,122.7C800,139,880,117,960,128C1040,139,1120,181,1200,197.3C1280,213,1360,203,1400,197.3L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+                </svg>
+            </div>              
                 </div>
             </section>
+            
             <section className="bg-white dark:bg-gray-900">
                 <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 <div className="mr-auto flex flex-col justify-center lg:col-span-7">
@@ -30,7 +37,8 @@ const Home = () => {
                         </a>  
                     </div>
                     <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                    <img src={projectImage} alt="Project" />
+                    <img src={heroImage} alt="Hero" />
+
                     </div>                
                 </div>
             </section>
